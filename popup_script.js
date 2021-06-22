99faf9e2bf61d223786da1fa42f2d17bc2711935
AshQTan <40342001+AshQTan@users.noreply.github.com>
2021-06-22 14:45:47 -0700

    first draft

---
+++ popup_script.js
@@ -0,0 +1,8 @@
+console.log("pop-up script");
+chrome.runtime.sendMessage({
+    message: "get_name"
+}, response => {
+    if (response.message === 'success'){
+        document.querySelector('div').innerHTML = `Hello ${response.payload}`;
+    }
+});
